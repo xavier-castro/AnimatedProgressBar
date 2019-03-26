03bcf126cfdc6e28b831acb774e9887f97c216cf
xavier-castro <xavier_castro@icloud.com>
2019-03-25 19:16:20 -0500

    Add animation

--- AnimatedProgressBar/ViewController.swift
+++ AnimatedProgressBar/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	let shapeLayer = CAShapeLayer()
+	let trackLayer = CAShapeLayer()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		view.backgroundColor = .white
+
+		let center = view.center
+
+		let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+
+		trackLayer.path = circularPath.cgPath
+		trackLayer.strokeColor = UIColor.lightGray.cgColor
+		trackLayer.fillColor = UIColor.clear.cgColor
+		trackLayer.lineWidth = 10
+		trackLayer.lineCap = .round
+		view.layer.addSublayer(trackLayer)
+
+		shapeLayer.path = circularPath.cgPath
+		shapeLayer.strokeColor = UIColor.red.cgColor
+		shapeLayer.fillColor = UIColor.clear.cgColor
+		shapeLayer.lineWidth = 10
+		shapeLayer.lineCap = .round
+		shapeLayer.strokeEnd = 0
+
+		view.layer.addSublayer(shapeLayer)
+
+		view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+	}
+
+	@objc private func handleTap() {
+		let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+		basicAnimation.toValue = 1
+		basicAnimation.duration = 2
+		basicAnimation.fillMode = .forwards
+		basicAnimation.isRemovedOnCompletion = false
+		shapeLayer.add(basicAnimation, forKey: "basicId")
 	}
 
 
